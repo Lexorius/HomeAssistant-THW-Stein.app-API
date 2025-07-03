7396cce8bba0bce885aac7ff72cebef19dd59350
Thomas Kloppholz <lexorius@web.de>
2025-07-03 02:09:04 +0200

    Add files via upload
    v0.00.2

---
+++ custom_components/thw_stein/config_flow.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+import voluptuous as vol
+from homeassistant import config_entries
+from homeassistant.core import callback
+from .const import DOMAIN, CONF_USERNAME, CONF_PASSWORD, CONF_BUNAME, CONF_SCAN_INTERVAL, DEFAULT_SCAN_INTERVAL
+
+DATA_SCHEMA = vol.Schema({
+    vol.Required(CONF_USERNAME): str,
+    vol.Required(CONF_PASSWORD): str,
+    vol.Required(CONF_BUNAME): str,
+    vol.Optional(CONF_SCAN_INTERVAL, default=DEFAULT_SCAN_INTERVAL): vol.All(vol.Coerce(int), vol.Range(min=15, max=3600)),
+})
+
+class SteinConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            return self.async_create_entry(title=user_input[CONF_BUNAME], data=user_input)
+        return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA, errors=errors)
+
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        return SteinOptionsFlowHandler(config_entry)
+
+class SteinOptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        return self.async_show_form(step_id="init", data_schema=DATA_SCHEMA)
--- custom_components/thw_stein/const.py
+++ custom_components/thw_stein/const.py
@@ -3,4 +3,4 @@ CONF_USERNAME = "username"
 CONF_PASSWORD = "password"
 CONF_BUNAME = "buname"
 CONF_SCAN_INTERVAL = "scan_interval"
-DEFAULT_SCAN_INTERVAL = 60  # Sekunden
+DEFAULT_SCAN_INTERVAL = 60
