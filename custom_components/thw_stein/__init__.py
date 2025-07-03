018e93d883d2d53e61aba8bf60dcbb26cf002802
Thomas Kloppholz <lexorius@web.de>
2025-07-04 01:25:48 +0200

    Update __init__.py
    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS) added

--- custom_components/thw_stein/__init__.py
+++ custom_components/thw_stein/__init__.py
@@ -49,6 +49,6 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         "client": client,
         "coordinator": coordinator,
     }
-
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    #hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
